2dcb9743a6ac9db110b2798b63e00af3e8b3467e
MicSianat <michaelsiakuva@gmail.com>
2024-11-19 23:04:25 +0200

    week 3 assignment

---
+++ week 3 assignment.py
@@ -0,0 +1,19 @@
+def calculate_discount(price, discount_percent):
+  if discount_percent >= 20:
+    return price - (price * (discount_percent/100))
+  else:
+   return price
+
+#prompt the user for input
+try:
+    original_price = float(input("Enter the original price of the item: "))
+    discount_percentage = float(input("Enter the discount percentage: ")) 
+    #calculate the final price
+    final_price = calculate_discount(original_price, discount_percentage)   
+    #print results
+    if discount_percentage >= 20:
+       print(f"The final price after discount is  :${final_price:.2f}")    
+    else:
+       print(f"No discount applied. The original price is: ${original_price:.2f}")    
+except ValueError:
+   print("Invalid input. Please enter a numeric value for the price and discount.")                
